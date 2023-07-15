141571be7c8dfbef4650259624c0c009814d6daf
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-07-15 06:28:18 +0530

    Added Linked List Implementation in Python

---
+++ nodeCreation.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+a = Node(13)
+b = Node(15)
+
+a.next = b
+
+print(a.data)
+print(b.data)
+print(a.next.data)
+print(a.next)
+#print(a)
+print(b)
---
+++ printLinkedList.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None #pointer to the next node
+
+
+'''If we are able to get head of a Linked list then we can tranverse it very easily'''
+
+
+def printLinkedList(head): #this head gets passed from the TakeInput function
+
+    while head is not None:
+        print(str(head.data) + "=>", end='')
+        head = head.next
+    print('None')
+    return
+
+
+
+def takeInput():
+
+    inputLs = [int(ele) for ele in input().split()] #returns a list
+    head = None
+    #our break point is -1, if we encounter -1 that means element before that is the last element of linked list
+    for currEle in inputLs:
+        if currEle == -1:
+            break
+
+        newNode = Node(currEle)
+        if head is None: #will only get set in the first run of the loop
+            head = newNode #once our head is updated we need to create connections in linked lists
+        else:
+            curr = head
+
+            while curr.next is not None:
+                curr = curr.next #this will update the current to last connected node
+            curr.next = newNode #will help in connecting the latest additon to linked list
+    return head
+
+headOfLinkedList = takeInput()
+print(headOfLinkedList) 
+printLinkedList(headOfLinkedList)
