bc3e37956c2f7d73a9a239b049e6f7cad36a3d71
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2023-07-28 00:29:51 +0530

    Deleting Data in Doubly LL

---
+++ deleteDataInDoublyLL.py
@@ -0,0 +1,78 @@
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+
+def takeInput():
+
+    inputLs = [int(ele) for ele in input().split()]
+    head = None
+    tail = None
+    for currEle in inputLs:
+
+        if currEle == -1:
+            break
+        
+        newNode = Node(currEle)
+
+        if head is None:
+            head = newNode
+            tail = newNode
+            
+        else:
+            newNode.prev = tail
+            tail.next = newNode
+            tail = newNode
+    return head, tail
+
+
+head, tail  = takeInput()
+
+def calculateLinkedListLength(head):
+    count = 0
+    while head is not None:
+        count += 1
+        head = head.next
+    return count
+
+def deleteData(head,data):
+    
+    if head.data == data:
+        head = head.next
+        head.prev = None
+        return head
+    else:
+        curr = head.next
+        prev = head
+        while curr is not None:
+            if curr.data == data:
+                prev.next = curr.next
+                curr = curr.next
+                if curr is not None:
+                    curr.prev = prev
+
+            prev = curr
+            curr = curr.next
+            
+        
+    
+    return head
+
+newHead = deleteData(head,3)
+
+
+
+def printLinkedList(head): #this head gets passed from the TakeInput function
+
+    while head is not None:
+        print(str(head.data) + "=>", end='')
+        head = head.next
+    print('None')
+    return
+
+
+printLinkedList(newHead)
